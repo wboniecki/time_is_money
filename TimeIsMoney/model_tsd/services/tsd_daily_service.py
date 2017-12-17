b2df187ab60dd2e20ddad837dc2b0e174704797c
Wiktor <wiktor.boniecki@gmail.com>
2017-12-17 21:54:12 +0100

    change daily stat data validation

--- TimeIsMoney/model_tsd/services/tsd_daily_service.py
+++ TimeIsMoney/model_tsd/services/tsd_daily_service.py
@@ -8,50 +8,65 @@ class TSDDailyService:
     def createOrUpdate(self, _item, _connected_realm, _data):
         today = datetime.date.today()
         tsd_hourly_service = TSDHourlyService()
+        daily_exist = True
         avg_market_price_sum = 0
+        tsd_len_mp = 0
         avg_price_sum = 0
+        tsd_len_avg =0
         avg_quantity_sum = 0
+        tsd_len_quan = 0
         tsd_hourly_set = tsd_hourly_service.getRealmDailyData(_item.id, _connected_realm.id, today)
         for tsd_hourly in tsd_hourly_set:
-            avg_market_price_sum += tsd_hourly.market_price
-            avg_price_sum += tsd_hourly.avg_price
-            avg_quantity_sum += tsd_hourly.quantity
+            if tsd_hourly.market_price > 0:
+                avg_market_price_sum += tsd_hourly.market_price
+                tsd_len_mp += 1
+            if tsd_hourly.avg_price > 0:
+                avg_price_sum += tsd_hourly.avg_price
+                tsd_len_avg += 1
+            if tsd_hourly.quantity > 0:
+                avg_quantity_sum += tsd_hourly.quantity
+                tsd_len_quan += 1
 
         tsd = self._getTSDDaily(today, _item.id, _connected_realm.id)
         if not tsd:
+            daily_exist = False
             tsd = ItemRealmTimeSeriesDataDaily()
-        if not tsd.date:
+        if not daily_exist:
             tsd.date = today
-        if not tsd.min_market_price or tsd.min_market_price > _data['market_price']:
+        if not daily_exist or tsd.min_market_price == 0 or tsd.min_market_price > _data['market_price']:
             tsd.min_market_price = _data['market_price']
         if tsd.max_market_price < _data['market_price']:
             tsd.max_market_price = _data['market_price']
 
-        if len(tsd_hourly_set) > 0:
-            tsd.avg_market_price = round(avg_market_price_sum/len(tsd_hourly_set), 4)
+        if daily_exist:
+            if tsd_len_mp > 0:
+                tsd.avg_market_price = round(avg_market_price_sum/tsd_len_mp, 4)
         else:
             tsd.avg_market_price = _data['market_price']
 
-        if not tsd.min_price or tsd.min_price > _data['min_price']:
+        if not daily_exist or tsd.min_price == 0 or tsd.min_price > _data['min_price']:
             tsd.min_price = _data['min_price']
         if tsd.max_price < _data['max_price']:
             tsd.max_price = _data['max_price']
 
-        if len(tsd_hourly_set) > 0:
-            tsd.avg_price = round(avg_price_sum/len(tsd_hourly_set), 4)
+        if daily_exist:
+            if tsd_len_avg > 0:
+                tsd.avg_price = round(avg_price_sum/tsd_len_avg, 4)
         else:
             tsd.avg_price = _data['avg_price']
 
-        if not tsd.open_market_price:
+        if not daily_exist or tsd.open_market_price == 0:
             tsd.open_market_price = _data['market_price']
-        tsd.end_market_price = _data['market_price']
-        if not tsd.min_quantity or tsd.min_quantity > _data['quantity']:
+        if _data['market_price'] > 0:
+            tsd.end_market_price = _data['market_price']
+        if not daily_exist or tsd.min_quantity > _data['quantity']:
             tsd.min_quantity = _data['quantity']
         if tsd.max_quantity < _data['quantity']:
             tsd.max_quantity = _data['quantity']
 
-        if len(tsd_hourly_set) > 0:
-            tsd.avg_quantity = round(avg_quantity_sum/len(tsd_hourly_set), 4)
+        if daily_exist:
+            if tsd_len_quan > 0:
+                tsd.avg_quantity = int(avg_quantity_sum/tsd_len_quan)
         else:
             tsd.avg_quantity = _data['quantity']
 
